4cea3deb4344bfe690d559975a11908f250f899e
David Jones <unixmonkey1@gmail.com>
2014-08-21 09:38:46 -0400

    Add wicked_pdf code and supporting layout/template

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,17 @@
 class PagesController < ApplicationController
 
   def issue_327
-
+    respond_to do |format|
+      format.pdf do
+        render :pdf => 'oferta',
+          :template => 'templates/show_offer.pdf.erb',
+          :layout => "layouts/templates.html.erb",
+          :print_media_type => true,
+          :page_size => "A4",
+          :disable_smart_shrinking => true,
+          :footer => { :right => '[page] of [topage]' }
+      end
+    end
   end
 
 end
